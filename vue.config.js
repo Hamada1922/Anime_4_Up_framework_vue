797d09aeeca1f2644bc107ede5e5b4bc3bb556fe
Hamada1922 <webs7op@gmail.com>
2023-02-13 10:49:49 +0200

    last one

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,6 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
+  publicPath:
+    process.env.NODE_ENV === "production" ? "/Anime_4_Up_framework_vue/" : "/",
   transpileDependencies: true,
 });
